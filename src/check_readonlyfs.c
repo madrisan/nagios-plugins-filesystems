fb19a2a5fc82d53e49f42647acb733c0cc51bfed
Davide Madrisan <davide.madrisan@gmail.com>
2013-01-15 19:26:16 +0100

    check_readonlyfs: new option --list
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- src/check_readonlyfs.c
+++ src/check_readonlyfs.c
@@ -26,12 +26,43 @@
 #include <compat_getopt.h>
 #endif
 
+#if HAVE_MNTENT_H
+#include <mntent.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "mountlist.h"
 #include "nputils.h"
+
+/* Linked list of mounted file systems. */
+static struct mount_entry *mount_list;
+
+/* Linked list of file system types to display.
+ * If 'fs_select_list' is NULL, list all types.
+ * This table is generated dynamically from command-line options,
+ * rather than hardcoding into the program what it thinks are the
+ * valid file system types; let the user specify any file system type
+ * they want to, and if there are any file systems of that type, they
+ * will be shown.
+ *
+ * Some file system types:
+ * 4.2 4.3 ufs nfs swap ignore io vm efs dbg */
+
+static struct fs_type_list *fs_select_list;
+
+/* Linked list of file system types to omit.
+ *    If the list is empty, don't exclude any types.  */
+
+static struct fs_type_list *fs_exclude_list;
+
+/* If true, show only local file systems.  */
+static bool show_local_fs;
+
+/* If true, print file system type as well.  */
+static bool print_type;
 
 static void __attribute__ ((__noreturn__)) print_version (void)
 {
@@ -42,6 +73,7 @@ static void __attribute__ ((__noreturn__)) print_version (void)
 static struct option const longopts[] = {
   {(char *) "help", no_argument, NULL, 'h'},
   {(char *) "version", no_argument, NULL, 'V'},
+  {(char *) "list", no_argument, NULL, 'L'},
   {NULL, 0, NULL, 0}
 };
 
@@ -52,6 +84,7 @@ check for readonly filesystems\n\
 Copyright (C) 2013 Davide Madrisan <" PACKAGE_BUGREPORT ">\n", out);
   fputs ("\n\
   Usage:\n\
+\t" PACKAGE_NAME " --list\n\
 \t" PACKAGE_NAME " --help\n\
 \t" PACKAGE_NAME " --version\n\n", out);
 
@@ -62,13 +95,20 @@ int
 main (int argc, char **argv)
 {
   int c, status = STATE_UNKNOWN;
+  struct mount_entry *me;
+  bool print_mountedfs = false;
+  fs_exclude_list = NULL;
+  print_type = false;
 
-  while ((c = getopt_long (argc, argv, "hV", longopts, NULL)) != -1)
+  while ((c = getopt_long (argc, argv, "LhV", longopts, NULL)) != -1)
     {
       switch (c)
 	{
 	default:
 	  usage (stderr);
+	  break;
+	case 'L':
+	  print_mountedfs = true;
 	  break;
 	case 'h':
 	  usage (stdout);
@@ -79,6 +119,27 @@ main (int argc, char **argv)
 	}
     }
 
+  mount_list =
+    read_file_system_list ((fs_select_list != NULL
+			    || fs_exclude_list != NULL
+			    || print_type || show_local_fs));
+
+  if (NULL == mount_list)
+    {
+      /* Couldn't read the table of mounted file systems. */
+      perror ("cannot read table of mounted file systems");
+    }
+
+  if (print_mountedfs)
+    {
+      me = mount_list;
+      fprintf (stdout, "List of checked filesystems:\n");
+      while (me)
+	{
+	  fprintf (stdout, " %s (%s)\n", me->me_mountdir, me->me_type);
+	  me = me->me_next;
+	}
+    }
 
   return status;
 }
--- src/mountlist.c
+++ src/mountlist.c
@@ -1,4 +1,143 @@
 #include "config.h"
 
-#include "mountlist.h"
+#ifdef MOUNTED_GETMNTENT1
+# include <mntent.h>
+# if !defined MOUNTED
+#  if defined _PATH_MOUNTED	/* GNU libc  */
+#   define MOUNTED _PATH_MOUNTED
+#  endif
+# endif
+#endif
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mountlist.h"
+#include "xalloc.h"
+
+#ifndef ME_DUMMY
+# define ME_DUMMY(Fs_name, Fs_type)             \
+    (strcmp (Fs_type, "autofs") == 0            \
+     || strcmp (Fs_type, "none") == 0           \
+     || strcmp (Fs_type, "proc") == 0           \
+     || strcmp (Fs_type, "subfs") == 0          \
+     /* for NetBSD 3.0 */                       \
+     || strcmp (Fs_type, "kernfs") == 0         \
+     /* for Irix 6.5 */                         \
+     || strcmp (Fs_type, "ignore") == 0)
+#endif
+
+#ifndef ME_REMOTE
+/* A file system is "remote" if its Fs_name contains a ':'
+ *    or if (it is of type (smbfs or cifs) and its Fs_name starts with '//').  */
+# define ME_REMOTE(Fs_name, Fs_type)            \
+    (strchr (Fs_name, ':') != NULL              \
+     || ((Fs_name)[0] == '/'                    \
+         && (Fs_name)[1] == '/'                 \
+         && (strcmp (Fs_type, "smbfs") == 0     \
+             || strcmp (Fs_type, "cifs") == 0)))
+#endif
+
+#if defined MOUNTED_GETMNTENT1 || defined MOUNTED_GETMNTENT2
+
+/* Return the device number from MOUNT_OPTIONS, if possible.
+ *    Otherwise return (dev_t) -1.  */
+static dev_t
+dev_from_mount_options (char const *mount_options)
+{
+  /* GNU/Linux allows file system implementations to define their own
+   *      meaning for "dev=" mount options, so don't trust the meaning
+   *           here.  */
+# ifndef __linux__
+
+  static char const dev_pattern[] = ",dev=";
+  char const *devopt = strstr (mount_options, dev_pattern);
+
+  if (devopt)
+    {
+      char const *optval = devopt + sizeof dev_pattern - 1;
+      char *optvalend;
+      unsigned long int dev;
+      errno = 0;
+      dev = strtoul (optval, &optvalend, 16);
+      if (optval != optvalend
+	  && (*optvalend == '\0' || *optvalend == ',')
+	  && !(dev == ULONG_MAX && errno == ERANGE) && dev == (dev_t) dev)
+	return dev;
+    }
+
+# endif
+  (void) mount_options;
+  return -1;
+}
+
+#endif
+
+/* Return a list of the currently mounted file systems, or NULL on error.
+ *    Add each entry to the tail of the list so that they stay in order.
+ *       If NEED_FS_TYPE is true, ensure that the file system type fields in
+ *          the returned list are valid.  Otherwise, they might not be.  */
+struct mount_entry *
+read_file_system_list (bool need_fs_type)
+{
+  struct mount_entry *mount_list;
+  struct mount_entry *me;
+  struct mount_entry **mtail = &mount_list;
+  (void) need_fs_type;
+
+#ifdef MOUNTED_GETMNTENT1	/* GNU/Linux, 4.3BSD, SunOS, HP-UX, Dynix, Irix.  */
+  {
+    struct mntent *mnt;
+    char const *table = MOUNTED;
+    FILE *fp;
+
+    fp = setmntent (table, "r");
+    if (fp == NULL)
+      return NULL;
+
+    while ((mnt = getmntent (fp)))
+      {
+	me = xmalloc (sizeof *me);
+	me->me_devname = xstrdup (mnt->mnt_fsname);
+	me->me_mountdir = xstrdup (mnt->mnt_dir);
+	me->me_type = xstrdup (mnt->mnt_type);
+	me->me_type_malloced = 1;
+	me->me_dummy = ME_DUMMY (me->me_devname, me->me_type);
+	me->me_remote = ME_REMOTE (me->me_devname, me->me_type);
+	me->me_dev = dev_from_mount_options (mnt->mnt_opts);
+
+	/* Add to the linked list. */
+	*mtail = me;
+	mtail = &me->me_next;
+      }
+
+    if (endmntent (fp) == 0)
+      goto free_then_fail;
+  }
+#endif /* MOUNTED_GETMNTENT1. */
+
+  *mtail = NULL;
+  return mount_list;
+
+free_then_fail:
+  {
+    int saved_errno = errno;
+    *mtail = NULL;
+
+    while (mount_list)
+      {
+	me = mount_list->me_next;
+	free (mount_list->me_devname);
+	free (mount_list->me_mountdir);
+	if (mount_list->me_type_malloced)
+	  free (mount_list->me_type);
+	free (mount_list);
+	mount_list = me;
+      }
+
+    errno = saved_errno;
+    return NULL;
+  }
+}

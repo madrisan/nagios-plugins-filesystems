f8c82ee83b8e783e94257d94c9f77ecaf88b77b5
madrisan <davide.madrisan@gmail.com>
2013-02-09 20:45:08 +0100

    mountlist: add copyleft notice
    Signed-off-by: madrisan <davide.madrisan@gmail.com>

--- lib/mountlist.c
+++ lib/mountlist.c
@@ -1,3 +1,26 @@
+/*
+ * License: GPL
+ * Copyright (c) 2013 Davide Madrisan <davide.madrisan@gmail.com>
+ *
+ * A Nagios plugin to check for readonly filesystems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This library was largely based on and inspired by the coreutils code.
+ */
+
 #include "config.h"
 
 #include <errno.h>
